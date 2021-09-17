9a8a66515e8b09a5fb77e337dcf79d8cefa73437
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-09-17 21:17:41 +0200

    Add actions types & creators in filterRedux

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -10,11 +10,15 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
+export const FILTERS = createActionName('FILTERS');
+export const TAGS = createActionName('TAGS');
 // TODO - add other action types
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
+export const filters = payload => ({ payload, type: FILTERS })
+export const tags = payload => ({ payload, type: TAGS })
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
