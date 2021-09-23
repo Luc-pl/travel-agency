c1935b0dd2e04e46b8c39874bdf2190427e7582f
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-09-23 23:05:04 +0200

    Add new Order

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,9 +3,20 @@ import PropTypes from 'prop-types';
 //import styles from './OrderForm.scss';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../../features/OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
+//import {formatPrice} from '../../../utils/formatPrice';
+//import {calculateTotal} from '../../../utils/calculateTotal';
 
-const OrderForm = ({tripCost, options}) => (
+
+
+const OrderForm = ({tripCost, options, setOrderOption}) => (
     <Row>
+      {pricing.map(({...option}) => (
+        <Col key={option.id} md={4}>
+          <OrderOption setOrderOption={setOrderOption} currentValue={options[options.id]} {...options}/>
+        </Col>
+      ))}
       <Col xs={12}>
         <OrderSummary tripCost={tripCost} options={options}/>
       </Col>
@@ -15,7 +26,8 @@ const OrderForm = ({tripCost, options}) => (
 OrderForm.propTypes = {
     tripCost: PropTypes.string,
     options: PropTypes.object,
+    pricing: PropTypes.array,
+    setOrderOption: PropTypes.func,
 };
-
 
 export default OrderForm;
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,45 @@
+import styles from './OrderOption.module.scss';
+import PropTypes from 'prop-types';
+import OrderOptionDropdown from './OrderOptionDropdown';
+import OrderOptionIcons from './OrderOptionIcons';
+import OrderOptionNumber from './OrderOptionNumber';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes';
+import OrderOptionText from './OrderOptionText';
+import OrderOptionDate from './OrderOptionDate';
+
+const optionTypes = {
+    dropdown: OrderOptionDropdown,
+    icons: OrderOptionIcons,
+    checkboxes: OrderOptionCheckboxes,
+    number: OrderOptionNumber,
+    text: OrderOptionText,
+    date: OrderOptionDate,
+};
+
+const OrderOption = ({id, setOrderOption, name, type, ...otherProps}) => {
+    const OptionComponent = optionTypes[type];
+    if(!OptionComponent){
+      return null;
+    } else {
+      return (
+        <div className={styles.component}>
+          <h3 className={styles.title}>{name}</h3>
+          <OptionComponent
+            {...otherProps}
+            setOptionValue={value => setOrderOption({[id]: value})}
+            name={name}
+          />
+        </div>
+      );
+    }
+};
+
+
+OrderOption.propTypes = {
+    name: PropTypes.string,
+    setOrderOption: PropTypes.func,
+    type: PropTypes.node,
+};
+  
+
+export default OrderOption;
