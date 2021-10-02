03d327db16142741e12f0652adc5cb77ffbf5066
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-10-02 15:50:43 +0200

    Add test OrderOption

---
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import OrderOption from './OrderOption';
+import DatePicker from 'react-datepicker';
+
+describe('Component OrderOption', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<OrderOption type='Lorem ipsum' name='Lorem ipsum' />);
+    expect(component).toBeTruthy();
+    console.log(component.debug());
+  });
+
+  it('should return empty object if called without required props', () => {
+    const component = shallow(<OrderOption />);
+    expect(component).toEqual({});
+  });
+
+  it('shold render prop name in title', () => {
+    const name = 'options';
+    const type = 'number';
+    const title = 'title';
+    const component = shallow(<OrderOption type={type} name={name} title={title} />);
+    expect(component.find('.title').text()).toEqual(name);
+  });
+
+});
+
+// testowanie każdego z subkomponentów
+
+const optionTypes = {
+  dropdown: 'OrderOptionDropdown',
+  icons: 'OrderOptionIcons',
+  checkboxes: 'OrderOptionCheckboxes',
+  number: 'OrderOptionNumber',
+  text: 'OrderOptionText',
+  date: 'OrderOptionDate',
+};
+
+// dodanie mockowych propsów - opcje z pliku pricing.json
+
+const mockProps = {
+  id: 'abc',
+  name: 'Lorem',
+  values: [
+    {id: 'aaa', icon: 'h-square', name: 'Lorem A', price: 0},
+    {id: 'xyz', icon: 'h-square', name: 'Lorem X', price: 100},
+  ],
+  required: false,
+  currentValue: 'aaa',
+  price: '50%',
+  limits: {
+    min: 0,
+    max: 6,
+  },
+};
+
+const mockPropsForType = {
+  dropdown: {},
+  icons: {},
+  checkboxes: {currentValue: [mockProps.currentValue]},
+  number: {currentValue: 1},
+  text: {},
+  date: {},
+};
+
+const testValue = mockProps.values[1].id;
+const testValueNumber = 3;
+
+for(let type in optionTypes){
+  describe(`Component OrderOption with type=${type}`, () => {
+    /* test setup */ //każdy test będzie miał do dyspozycji świeżo wyrenderowany komponent OrderOption, i nie musimy używać funkcji shallow w każdym z testów!
+    let component;
+    let subcomponent;
+    let renderedSubcomponent;
+    let mockSetOrderOption; /* 1 */
+
+    beforeEach(() => {
+      mockSetOrderOption = jest.fn(); /* 2 */
+      component = shallow(
+        <OrderOption
+          type={type}
+          setOrderOption={mockSetOrderOption} /* 3 */
+          {...mockProps}
+          {...mockPropsForType[type]}
+        />
+      );
+      subcomponent = component.find(optionTypes[type]);
+      renderedSubcomponent = subcomponent.dive(); //dive pozwala na testowanie wszystkich subkomponentów
+    });
+    
+    /* common tests */
+    // it('passes dummy test', () => {
+    //   expect(1).toBe(1);
+    // });
+    it(`renders ${optionTypes[type]}`, () => {
+      expect(subcomponent).toBeTruthy();
+      expect(subcomponent.length).toBe(1);
+    });
+
+    /* type-specific tests */
+    switch (type) {
+      case 'dropdown': {
+        /* tests for dropdown */
+
+        /*w stałej emptyOption używamy selektora atrybutu, 
+        a w stałej options – metody .not. W ten sposób możemy wybrać, które opcje 
+        chcemy zapisać w każdej z tych stałych.*/
+
+        it('contains select and options', () => {
+          const select = renderedSubcomponent.find('select');
+          expect(select.length).toBe(1);
+    
+          const emptyOption = select.find('option[value=""]').length;
+          expect(emptyOption).toBe(1);
+    
+          const options = select.find('option').not('[value=""]');
+          expect(options.length).toBe(mockProps.values.length);
+          expect(options.at(0).prop('value')).toBe(mockProps.values[0].id);
+          expect(options.at(1).prop('value')).toBe(mockProps.values[1].id);
+        });
+
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent.find('select').simulate('change', {currentTarget: {value: testValue}});
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValue });
+        });
+        break;
+      }
+      
+      case 'icon': {
+        /* tests for icons */
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent.find('icon');
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+        });
+        break;
+      }
+
+      case 'checkboxes': {
+        /* tests for checkboxes */
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent.find({value: testValue}).simulate('change', {currentTarget: {checked: true}});
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: [mockProps.currentValue, testValue] });
+        });
+        break;
+      }
+
+      case 'number': {
+        /* tests for number */
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent.find('input').simulate('change', {currentTarget: {value: testValueNumber}});
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValueNumber });
+        });
+        break;
+      }
+
+      case 'text': {
+        /*test for text */
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent.find('input').simulate('change', {currentTarget: {value: testValue}});
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValue });
+        });
+        break;
+      }
+
+      case 'date': {
+        /*test for text */
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent.find(DatePicker).simulate('change', testValue);
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValue });
+        });
+        break;
+      }
+    }
+
+    
+
+  });
+}
